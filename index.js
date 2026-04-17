228c7d882f7de898d0beec18a231e1d624c7356a
Adama Asenath <169082427+HopeAda@users.noreply.github.com>
2026-04-17 19:07:37 +0100

    Wrote js logic for edit mode

--- index.js
+++ index.js
@@ -1,21 +1,61 @@
 const checkbox = document.querySelector("input.check");
 const title = document.querySelector(".title");
 const priorityBadge = document.querySelector(".priority");
+const priorityIndicator = document.querySelector(".priority-indicator");
 const description = document.querySelector(".desc");
+const descriptionToggle = document.querySelector(".toggle-desc");
+const descriptionCont = document.querySelector(".desc-container");
 const timeCont = document.querySelector(".time time");
-const remainingTime = document.querySelector(".time_remaining span");
+const remainingTime = document.querySelector(
+	".time_remaining span.time-remaining-indicator",
+);
+const overdueIndicator = document.querySelector(
+	".time_remaining span.overdue-indicator",
+);
+
+const statusCont = document.querySelector(".status-cont");
+const statusList = Array.from(document.querySelectorAll("#status-list li"));
 const statusBadge = document.querySelector(".status");
+const statusItems = ["pending", "in progress", "done"];
+
 const editButton = document.querySelector(".edit");
 const deleteButton = document.querySelector(".delete");
+
+const titleInput = document.querySelector(".edit-title");
+const descInput = document.querySelector(".edit-desc");
+const dateInput = document.querySelector(".edit-due-date");
+const timeInput = document.querySelector(".edit-due-time");
+const editPriorityContainer = document.querySelector(".edit-priority");
+const priorityInputDisplay = document.querySelector(".edit-priority-display");
+const priorityInputList = document.querySelector(".priority-list");
+const priorityOptions = Array.from(
+	document.querySelectorAll(".priority-list li"),
+);
+
+const priorityItems = ["high", "medium", "low"];
+
+const rootCard = document.querySelector("article.root");
+const editCard = document.querySelector("article.edit");
+
+const saveEdit = document.querySelector(".save-btn");
+const cancelEdit = document.querySelector(".cancel-btn");
+
+const form = document.querySelector("form");
 
 let taskInfo = {
 	title: "Complete Stage 0 Task",
 	desc: "Code a modern todo / task card component with semantic html tags, screen reader accessible elements and time remaining indicator",
-	priority: "high",
+	priority: "medium",
 	status: "pending",
-	"due-date": new Date(Date.now() + 300000), //5 minutes from current time
+	"due-date": new Date(Date.now() + 10000), //5 minutes from current time
 	complete: false,
 };
+
+let isLong = taskInfo.desc.length > 150 || taskInfo.desc.split("\n").length > 3;
+
+if (isLong) {
+	descriptionCont.classList.add("collapsed");
+}
 
 function buildComponent() {
 	checkbox.checked = taskInfo.complete;
@@ -23,8 +63,12 @@ function buildComponent() {
 	description.textContent = taskInfo.desc;
 	statusBadge.textContent = taskInfo.status;
 	statusBadge.className = `status badge ${taskInfo.status == "in progress" ? "progress" : taskInfo.status.toLowerCase()}`;
+
+	descriptionToggle.style.display = isLong ? "block" : "none";
+
 	priorityBadge.textContent = taskInfo.priority;
 	priorityBadge.className = `priority badge ${taskInfo.priority}`;
+	priorityIndicator.className = `priority-indicator  ${taskInfo.priority}`;
 	timeCont.setAttribute("datetime", taskInfo["due-date"].toISOString());
 	timeCont.textContent = taskInfo["due-date"].toLocaleDateString("en-US", {
 		day: "numeric",
@@ -44,9 +88,12 @@ checkbox.addEventListener("change", (event) => {
 	if (taskInfo.complete) {
 		taskInfo.status = "Done";
 		clearInterval(timer);
-		checkRemaining();
+		msg = "Completed";
+		remainingTime.style.display = "block";
+		overdueIndicator.style.display = "none";
+		remainingTime.textContent = msg;
 	} else {
-		taskInfo.status = "in progress";
+		taskInfo.status = "pending";
 		timer = setInterval(checkRemaining, 60000);
 		checkRemaining();
 	}
@@ -58,10 +105,59 @@ title.addEventListener("click", () => {
 });
 
 statusBadge.addEventListener("click", () => {
-	if (taskInfo.status == "pending") {
-		taskInfo.status = "in progress";
-		buildComponent();
+	const isOpen = statusCont.classList.contains("open");
+	statusCont.classList.toggle("open");
+	statusCont.setAttribute("aria-expanded", !isOpen);
+
+	if (!isOpen) {
+		statusList.forEach((itm) => {
+			itm.setAttribute("tabindex", "0");
+		});
+		statusList[0].focus();
+	} else {
+		statusList.forEach((itm) => {
+			itm.setAttribute("tabindex", "-1");
+		});
 	}
+});
+
+statusBadge.addEventListener("keydown", (e) => {
+	if (e.key === "Enter" || e.key === " ") {
+		e.preventDefault();
+		statusBadge.click();
+	}
+});
+
+let status = taskInfo.status;
+statusList.forEach((itm, id) => {
+	itm.addEventListener("click", () => {
+		let prevStat = status;
+
+		status = statusItems[id];
+
+		taskInfo.status = status;
+
+		statusBadge.click();
+
+		if (status === "done") {
+			checkbox.checked = true;
+		} else {
+			checkbox.checked = false;
+		}
+
+		checkbox.dispatchEvent(new Event("change"));
+
+		statusCont.classList.remove("open");
+		statusCont.setAttribute("aria-expanded", "false");
+		buildComponent();
+	});
+
+	itm.addEventListener("keydown", (e) => {
+		if (e.key === "Enter" || e.key === " ") {
+			e.preventDefault();
+			itm.click();
+		}
+	});
 });
 
 function updateTimerMsg(timeRemaining) {
@@ -89,13 +185,14 @@ function updateTimerMsg(timeRemaining) {
 	}
 
 	if (!timeRemaining[3]) {
-		remainingTime.style.color =
-			amount == 0 ? "var(--text-secondary)" : "#d91b24";
+		overdueIndicator.textContent = msg;
+		overdueIndicator.style.display = "block";
+		remainingTime.style.display = "none";
 	} else {
-		remainingTime.style.color = "var(--text-secondary)";
+		remainingTime.textContent = msg;
+		remainingTime.style.display = "block";
+		overdueIndicator.style.display = "none";
 	}
-
-	remainingTime.textContent = msg;
 }
 
 function checkRemaining() {
@@ -121,10 +218,102 @@ checkRemaining();
 
 let timer = setInterval(checkRemaining, 60000);
 
+descriptionToggle.addEventListener("click", () => {
+	if (descriptionCont.classList.contains("collapsed")) {
+		descriptionCont.classList.remove("collapsed");
+		descriptionToggle.textContent = "Collapse";
+		descriptionCont.setAttribute("aria-expanded", true);
+	} else {
+		descriptionCont.classList.add("collapsed");
+		descriptionToggle.textContent = "Expand";
+		descriptionCont.setAttribute("aria-expanded", false);
+	}
+});
+
 editButton.addEventListener("click", () => {
-	alert("Edit Button Clicked!");
+	titleInput.value = taskInfo.title;
+	descInput.value = taskInfo.desc;
+	dateInput.value = taskInfo["due-date"].toISOString().split("T")[0];
+	timeInput.value = taskInfo["due-date"].toTimeString().slice(0, 5);
+
+	rootCard.style.display = "none";
+	editCard.style.display = "flex";
+	titleInput.focus();
 });
 
 deleteButton.addEventListener("click", () => {
 	alert("Delete Button Clicked!");
 });
+
+priorityInputDisplay.addEventListener("click", function () {
+	const isOpen = editPriorityContainer.classList.contains("open");
+	editPriorityContainer.classList.toggle("open");
+	editPriorityContainer.setAttribute("aria-expanded", !isOpen);
+	if (!isOpen) {
+		priorityOptions.forEach((itm) => {
+			itm.setAttribute("tabindex", "0");
+		});
+		priorityOptions[0].focus();
+	} else {
+		priorityOptions.forEach((itm) => {
+			itm.setAttribute("tabindex", "-1");
+		});
+	}
+});
+
+priorityInputDisplay.addEventListener("keydown", (e) => {
+	if (e.key === "Enter" || e.key === " ") {
+		e.preventDefault();
+		priorityInputDisplay.click();
+	}
+});
+
+let priority = taskInfo.priority;
+priorityInputDisplay.textContent = priority;
+priorityOptions.forEach((itm, id) => {
+	itm.addEventListener("click", () => {
+		priority = priorityItems[id];
+		priorityInputDisplay.textContent = priority;
+		priorityInputDisplay.click();
+	});
+	itm.addEventListener("keydown", (e) => {
+		if (e.key === "Enter" || e.key === " ") {
+			e.preventDefault();
+			itm.click();
+		}
+	});
+});
+
+saveEdit.onclick = function (event) {
+	event.preventDefault();
+
+	taskInfo.title =
+		titleInput.value.trim() != ""
+			? titleInput.value.trim()
+			: taskInfo.title;
+	taskInfo.desc =
+		descInput.value.trim() != "" ? descInput.value.trim() : taskInfo.desc;
+	taskInfo.priority = priority;
+
+	if (dateInput.value != "" || timeInput.value != "") {
+		let dateObj = new Date(
+			`${dateInput.value != "" ? dateInput.value : new Date().toISOString().split("T")[0]}T${timeInput.value || "00:00:00"}`,
+		);
+		taskInfo["due-date"] = dateObj;
+	}
+
+	buildComponent();
+	checkRemaining();
+	form.reset();
+	rootCard.style.display = "flex";
+	editCard.style.display = "none";
+	editButton.focus({ focusVisible: true });
+};
+
+cancelEdit.onclick = function (event) {
+	event.preventDefault();
+	form.reset();
+	rootCard.style.display = "flex";
+	editCard.style.display = "none";
+	editButton.focus({ focusVisible: true });
+};
